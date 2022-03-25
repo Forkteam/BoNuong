6277c7ceb62d23add9253dc3a5d3b45092c24518
nhuthuynhphu <nhuthuynhphu@github.com>
2022-03-26 03:23:02 +0700

    feature: cart

---
+++ BoNuong/Models/GioHang.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BoNuong.Models
+{
+    public class GioHang
+    {
+        BoNuongContext data = new BoNuongContext();
+        [Display(Name = "Mã sản phẩm")]
+        public int MaSP { get; set; }
+
+        [Display(Name = "Tên sản phẩm")]
+        public string Ten { get; set; }
+
+        [Display(Name = "Ảnh")]
+        public string Hinh { get; set; }
+
+        [Display(Name = "Giá bán")]
+        public double Gia { get; set; }
+
+        [Display(Name = "Giảm giá")]
+        public double GiamGia { get; set; }
+
+        [Display(Name = "Số lượng")]
+        public int SoLuong { get; set; }
+
+        [Display(Name = "Thành tiền")]
+        public double ThanhTien
+        {
+            get { return SoLuong * (Gia - GiamGia); }
+        }
+
+        public GioHang(int id)
+        {
+            MaSP = id;
+            SanPham sp = data.SanPham.Single(n => n.MaSP == MaSP);
+            Ten = sp.Ten;
+            Hinh = sp.Hinh;
+            Gia = double.Parse(sp.Gia.ToString());
+            SoLuong = 1;
+            GiamGia = 0;
+        }
+    }
+}
