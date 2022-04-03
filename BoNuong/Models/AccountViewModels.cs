b671577620c13adf5f24e6e2f1b254f09d1ba463
nhuthuynhphu <nhuthuynhphu@github.com>
2022-04-03 23:33:46 +0700

    feature: login with google

--- BoNuong/Models/AccountViewModels.cs
+++ BoNuong/Models/AccountViewModels.cs
@@ -8,6 +8,18 @@ namespace BoNuong.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
     }
 
     public class ExternalLoginListViewModel
